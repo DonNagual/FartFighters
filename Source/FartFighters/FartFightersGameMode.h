c026f13c272f6b199c47ed4d1c5ae485800b89b2
DonNagual <stonedbull@gmail.com>
2025-09-17 17:10:00 +0200

    First commit with .gitignore file

---
+++ Source/FartFighters/FartFightersGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FartFightersGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFartFightersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFartFightersGameMode();
+};
+
+
+
